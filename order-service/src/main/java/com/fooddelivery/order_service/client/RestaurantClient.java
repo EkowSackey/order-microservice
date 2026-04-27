7daab195a61d91189caa00f8f1cbcd9e883e6e31
Ekow Nii Saki Sackey <ekow.sackey@amalitech.com>
2026-04-27 09:39:10 +0000

    Epic 3: Enable Eureka Client and refactor Feign Clients

--- order-service/src/main/java/com/fooddelivery/order_service/client/RestaurantClient.java
+++ order-service/src/main/java/com/fooddelivery/order_service/client/RestaurantClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // Placeholder values, the name and URL will depend on your setup (e.g., using Eureka or direct URL)
-@FeignClient(name = "restaurant-service", url = "${restaurant-service.url:http://localhost:8082}")
+@FeignClient(name = "restaurant-service")
 public interface RestaurantClient {
 
     @GetMapping("/api/restaurants/{id}")
